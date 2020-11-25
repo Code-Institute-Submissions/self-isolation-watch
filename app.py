604b8c41116229fdabb8ecd03ab7e1d092f5b04b
mayasaffron <mayasaffronhan@hotmail.com>
2020-11-25 13:46:30 +0000

    created the login page, for existing users

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_symptoms")
 def get_symptoms():
-    symptoms = mongo.db.symptoms.find()
+    symptoms = list(mongo.db.symptoms.find())
     return render_template("symptoms.html", symptoms=symptoms)
 
 
@@ -31,7 +31,7 @@ def register():
                         {"username": request.form.get("username").lower()})
         if existing_user:
             flash("This username is already in use!")
-            return redirect(url_for("register"))
+            return redirect(url_for("login"))
     # check the passwords
         if request.form.get("password") == request.form.get("confirmpassword"):
             register = {
@@ -47,6 +47,41 @@ def register():
             flash("Passwords do not match! Please try again")
             return redirect(url_for("register"))
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+                        {"username": request.form.get("username").lower()})
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome back, {}".format(request.form.get("username")))
+                return redirect(url_for("get_symptoms", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect username and/or Password")
+                return redirect(url_for("login"))
+        else:
+            # username doesn't exist
+            flash("Incorrect username and/or Password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
+
+
+@app.route("/add_symptom", methods=["GET", "POST"])
+def add_symptom():
+    return render_template("add-symptom.html")
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("get_symptoms"))
 
 
 if __name__ == "__main__":
