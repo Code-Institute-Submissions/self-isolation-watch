515599f691f554929a299a2e7736ebc8adc1506f
mayasaffron <mayasaffronhan@hotmail.com>
2020-11-25 12:26:31 +0000

    created the register page, users will register and then add their symptoms

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import(
     redirect, request, session, url_for)
 if os.path.exists("env.py"):
     import env
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -15,11 +16,37 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_symptoms")
 def get_symptoms():
     symptoms = mongo.db.symptoms.find()
     return render_template("symptoms.html", symptoms=symptoms)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+                        {"username": request.form.get("username").lower()})
+        if existing_user:
+            flash("This username is already in use!")
+            return redirect(url_for("register"))
+    # check the passwords
+        if request.form.get("password") == request.form.get("confirmpassword"):
+            register = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(
+                    request.form.get("password"))
+            }
+            mongo.db.users.insert_one(register)
+        # put the new user into 'session' cookie
+            session["user"] = request.form.get("username").lower()
+            flash("Registration Successful!")
+        else:
+            flash("Passwords do not match! Please try again")
+            return redirect(url_for("register"))
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
