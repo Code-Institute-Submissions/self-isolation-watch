b9b4b36bc62e46d09da6b4b6b8e8566df964c691
mayasaffron <mayasaffronhan@hotmail.com>
2020-11-25 12:25:48 +0000

    created and added to script.js file, with materialize code initialising script

---
+++ static/js/scripts.js
@@ -0,0 +1,15 @@
+/* initialisation for mobile nav bar */
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+/* initialisation for carousel on welcome page */
+    $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+/* initialisation for date picker on profile form */
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
