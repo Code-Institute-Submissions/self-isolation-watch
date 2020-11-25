6b0de4b6b0a28c4956a7623d46643d7d7e69f2b3
mayasaffron <mayasaffronhan@hotmail.com>
2020-11-25 13:47:09 +0000

    created the add-symptoms view and form

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -11,5 +11,12 @@
 
 /* initialisation for date picker on profile form */
   $(document).ready(function(){
-    $('.datepicker').datepicker();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n:{
+            done: "select"
+        }
+    });
   });
