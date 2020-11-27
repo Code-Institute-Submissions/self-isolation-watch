ce90bffa5f64480d0c35f9421c74fa39dc5a42b1
mayasaffron <mayasaffronhan@hotmail.com>
2020-11-27 18:41:48 +0000

    added the search symptoms view and html

--- app.py
+++ app.py
@@ -24,6 +24,13 @@ def get_symptoms():
     return render_template("symptoms.html", symptoms=symptoms)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    symptoms = list(mongo.db.symptoms.find({"$text": {"$search": query}}))
+    return render_template("symptoms.html", symptoms=symptoms)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
