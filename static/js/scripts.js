1fc13af8d91bee034eb1ac08d43fe973715fb545
mayasaffron <mayasaffronhan@hotmail.com>
2020-12-13 19:16:10 +0000

    formatted script.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,57 +1,61 @@
-/* initialisation for mobile nav bar */
- $(document).ready(function(){
+// initialisation for mobile nav bar //
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
+});
 
-/* initialisation for date picker on new symptom form */
-  $(document).ready(function(){
+// initialisation for date picker on new symptom form //
+$(document).ready(function () {
     $('.datepicker').datepicker({
         format: "dd mmmm, yyyy",
         yearRange: 3,
         showClearBtn: true,
         autoClose: true,
-        i18n:{
+        i18n: {
             done: "select"
         }
     }
-    )});
-/* initialisation for select tool on new symptom  form */
-  $(document).ready(function(){
+    )
+});
+
+// initialisation for select tool on new symptom  form //
+$(document).ready(function () {
     $('select').formSelect();
-  })
+})
 
-/* initialisation for select tool on collapsible function for list of symptoms on symptom landing page */
- $(document).ready(function(){
+// initialisation for select tool on collapsible function for list of symptoms on symptom landing page //
+$(document).ready(function () {
     $('.collapsible').collapsible();
-     })
-/* initialisation for dropdown tool on for list of symptoms on my_symptoms page */     
-  $('.dropdown-trigger').dropdown();
+})
 
+// initialisation for dropdown tool on for list of symptoms on my_symptoms page //     
+$('.dropdown-trigger').dropdown();
 
-/* initialisation for modals for double checking deleting */  
-  $(document).ready(function(){
+// initialisation for modals for double checking deleting //  
+$(document).ready(function () {
     $('.modal').modal();
-  });
+});
 
-/* copying the site URL */ 
+// copying the site URL // 
 
 let clipboard = new ClipboardJS('.copy-btn');
-// If copy to clipboard function is successful
-clipboard.on('success', function(e) {
+// If copy to clipboard function is successful //
+clipboard.on('success', function (e) {
     $('.copy-btn').append('<p class="small-titles">Copied!</p>');
 });
-// If copy to clipboard function is not supported by users' browser, an error is triggered here
-clipboard.on('error', function(e) {
-alert("Oops, it looks like this function isn't supported on your browser! Don't worry, Just copy this: https://self-isolation-watch.herokuapp.com/");
+
+// If copy to clipboard function is not supported by users' browser, an error is triggered here //
+clipboard.on('error', function (e) {
+    alert("Oops, it looks like this function isn't supported on your browser! Don't worry, Just copy this: https://self-isolation-watch.herokuapp.com/");
 
 });
-// scroll to top
+// scroll to top //
 const scroller = document.querySelector(".scroller");
-window.addEventListener("scroll" , () => {
-    if (window.pageYOffset>100){
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
         scroller.classList.add("active");
-    } else {scroller.classList.remove("active");
-}
+    } else {
+        scroller.classList.remove("active");
+    }
 
 });
 
